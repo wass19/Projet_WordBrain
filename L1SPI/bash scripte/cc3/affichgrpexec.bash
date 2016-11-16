2faf13a21f1ab3736a4899964857d7dfca1b5325
samed oktay <spi2197@ic2s114-16.univ-lemans.fr>
2016-11-16 13:34:22 +0100

    jvous bz

--- L1SPI/bash scripte/cc3/affichgrpexec.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find ~/ -name $1 > toto
-b=`cat toto` 
-rm toto
-cd $b
-find ./ -perm -10 -print
-
--- L1SPI/bash scripte/entrainement cc3/affichparam.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i=1
-for param in $* ;do 
-	echo "parametre $i = $param"
-	i=`expr $i + 1`
-done
-
