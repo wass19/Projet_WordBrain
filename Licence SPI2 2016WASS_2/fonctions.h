ec6b9c80a8fc56b032a1e99da7e8e98fcb026b6a
wass19 <setif7219@gmail.com>
2016-11-28 21:09:14 +0100

    suppr ancien fic pour laisser celui de la fusion entre mon fic et celui de samed

--- Licence SPI2 2016WASS_2/fonctions.h
+++
@@ -1,7 +0,0 @@
-
-int bChainesEgalesCompare1MotsAvec5Autres(char *TexteAcomparer,char *sTexte2,char *sTexte3,char *sTexte4,char *sTexte5,char *sTexte6);//strcmp 1mot avec 5autres.
-int bsansblanc(char *R);//on regarde si il y a que des espaces dans R comme caractère.
-char *tomberNiveau2(int mat[N][N],char *R){//faire tomber les lettres pour le niveau 2 (3 cases par ligne et par colonne).
-char *tomberNiveau3(int mat[N][N],char *R){//faire tomber les lettres pour le niveau 3 (4 cases par ligne et par colonne).
-char *tomberNiveau4(int mat[N][N],char *R){//faire tomber les lettres pour le niveau 4 (5 cases par ligne et par colonne). 	
-char *TrouverLeMot(char reponse[20],char mot1[10],char mot2[10],char mot3[10],char mot4[10],char mot5[10],int mat[N][N],char *R){//on remplace le mot qui est égal a la chaine de caractère "reponse" qui est saisie par l'utilisateur par des espaces.
